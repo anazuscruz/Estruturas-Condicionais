ab1d429745a2e04c181532c8424a65e078b99ae0
Suzana <anazus.cruz@gmail.com>
2024-05-17 14:24:25 -0300

    switch case continuação

---
+++ estruturas-condicionais-simples/src/SwitchCase1.java
@@ -0,0 +1,16 @@
+public class SwitchCase1 {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if(plano == "B") {
+            System.out.println("100 minutos de ligação");
+        }else if(plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e Instagram grátis");
+        }else if(plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e Instagram grátia");
+            System.out.println("5GB Youtube");
+        }
+    }
+}
